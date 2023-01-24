28760d66c2ec66211cc52569fe756ad6bf6b2de7
TomotakaSuzuki <suzukitomo0308@gmail.com>
2023-01-24 13:12:22 +0900

    update

--- members/resources/views/post/show.blade.php
+++ members/resources/views/post/show.blade.php
@@ -15,9 +15,31 @@
                 <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
                     <div class="mt-4">
                         <h1 class="text-lg text-gray-700 font-semibold hover:underline cursor-pointer">
-                            {{ $post->title }}
-                            <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
-                        </h1><hr class="w-full">
+                                {{ $post->title }}
+                        </h1>
+                        <hr class="w-full">
+                    </div>
+                <div class="flex justify-end mt-4">
+                    <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
+                    <form method="post" action="{{route('post.destroy', $post)}}">
+                        @csrf
+                        @method('delete')
+                        <x-primary-button class="bg-red-700 float-right ml-4" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+                    </form>
+                </div>
+                <div>
+                    <p class="mt-4 text-gray-600 py-4">{{$post->body}}</p>
+                    @if($post->image)
+                        <div>
+                            (画像ファイル：{{$post->image}})
+                        </div>
+                        <img src="{{ asset('storage/images/'.$post->image)}}" class="mx-auto" style="height:300px;">
+                    @endif
+                    <div class="text-sm font-semibold flex flex-row-reverse">
+                        <p> {{ $post->user->name}} • {{$post->created_at->diffForHumans()}}</p>
+                    </div>
+                </div>
+                </div>
                         <p class="mt-4 text-gray-600 py-4">{{$post->body}}</p>
                         @if($post->image)
                             <div>
